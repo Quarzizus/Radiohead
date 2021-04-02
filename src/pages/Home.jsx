25e9855f7408438aec60f7c9faa5824bf16e1a52
quarzizus <angelzizu06@gmail.com>
2021-04-02 18:43:29 -0500

    before hooks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,36 @@
 import React from "react";
 import "../styles/Home.scss";
 
-class Home extends React.Component {
-  render() {
-    return (
-      <section className="Home">
-        <article className="Home_init">
-          <h2 className="Home_init-title">Radiohead concert</h2>
-          <p className="Home_init-p">
-            Reserva tu ticket ahora mismo y de paso mira quién más comparte tu
-            pasión por el rock de los 90s y los 00. Reserva tu ticket ahora
-            mismo y de paso mira
-          </p>
-          <button className="Home_init-button">Start</button>
-        </article>
-      </section>
-    );
+const useIncreaseCount = (max) => {
+  const [count, setCount] = React.useState(0);
+  if (count > max) {
+    setCount(0);
   }
-}
+  return [count, setCount];
+};
+
+const Home = () => {
+  const [count, setCount] = useIncreaseCount(5);
+  return (
+    <section className="Home">
+      <article className="Home_init">
+        <h2 className="Home_init-title">Radiohead concert</h2>
+        <p className="Home_init-p">
+          Reserva tu ticket ahora mismo y de paso mira quién más comparte tu
+          pasión por el rock de los 90s y los 00. Reserva tu ticket ahora mismo
+          y de paso mira
+        </p>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+          className="Home_init-button"
+        >
+          Start: {count}
+        </button>
+      </article>
+    </section>
+  );
+};
 
 export default Home;
